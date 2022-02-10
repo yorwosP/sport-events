dcd87f45dec7ab1256bc718b4e1151521ce533fc
yorwosP <yorwos@icloud.com>
2022-02-10 20:49:08 +0200

    added window in AppDelegate, removed appearance:standard from navigation bar

--- Sport Events/AppDelegate.swift
+++ Sport Events/AppDelegate.swift
@@ -10,7 +10,8 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    //ios 12 needs this
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
